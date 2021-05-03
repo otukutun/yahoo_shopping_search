974c92734491c598b4b385c7d05ceadc6b9528f9
otukutun <orehaorz@gmail.com>
2021-05-03 22:13:52 +0900

    Add webmock and active_support gems.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
-require "bundler/setup"
-require "yahoo_shopping_search"
+require 'bundler/setup'
+require 'yahoo_shopping_search'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
